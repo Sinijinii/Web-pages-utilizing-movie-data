94dfe227739bbb5f23e5dc18bc4cb77872dbd69a
swybean <swybean@gmail.com>
2024-05-16 14:29:33 +0900

    Diagrams 폴더 생성

--- TMDB_API_movie_data/사용자검색영화_함수화.py
+++ TMDB_API_movie_data/사용자검색영화_함수화.py
@@ -22,17 +22,18 @@ class MovieInfoRetriever:
             search_results = search_response.json()['results']
             if not search_results:
                 return None  # 검색 결과가 없으면 None 반환
-            movie = search_results[0]  # 첫 번째 검색 결과만 사용
-            # 필요한 정보 가져오기
-            movie_id = movie['id']
-            movie_info = {
-                "title": movie['title'],
-                "movie_id": movie_id,
-                "poster_url": f"https://image.tmdb.org/t/p/w500/{movie['poster_path']}" if movie['poster_path'] else None,
-                "genres": self.get_movie_genres(movie_id),
-                "actors": self.get_movie_cast(movie_id)
-            }
-            return movie_info
+            movies_info = []
+            for movie in search_results:
+                movie_id = movie['id']
+                movie_info = {
+                    "title": movie['title'],
+                    "movie_id": movie_id,
+                    "poster_url": f"https://image.tmdb.org/t/p/w500/{movie['poster_path']}" if movie['poster_path'] else None,
+                    "genres": self.get_movie_genres(movie_id),
+                    "actors": self.get_movie_cast(movie_id)
+                }
+                movies_info.append(movie_info)
+            return movies_info
         else:
             print("영화 정보를 가져오는데 문제가 발생했습니다. 응답 코드:", search_response.status_code)
             return None
@@ -55,19 +56,17 @@ class MovieInfoRetriever:
             return [actor['name'] for actor in response.json().get('cast', [])[:5]]
         else:
             print("주연배우 정보를 가져오는데 문제가 발생했습니다.")
-            return []
 
 def search_and_save_movie_info(api_key, movie_title):
     movie_retriever = MovieInfoRetriever(api_key)
     movie_info = movie_retriever.get_movie_info(movie_title)
     if movie_info:
-        file_name = f"{movie_title}.json"  # 파일명을 영화 이름으로 지정
-        with open(file_name, "w", encoding="utf-8") as json_file:
-            json.dump(movie_info, json_file, ensure_ascii=False, indent=4)
+        for movie in movie_info:
+            file_name = f"{movie['title']}.json"  # 파일명을 영화 이름으로 지정
+            with open(file_name, "w", encoding="utf-8") as json_file:
+                json.dump(movie, json_file, ensure_ascii=False, indent=4)
     else:
         print(f"'{movie_title}'에 대한 영화 정보를 찾을 수 없습니다.")
 
 # 함수를 사용하여 영화 정보 검색 및 저장
-# API 뒤에 영화 이름 검색하면 되는데, 이름 정확하게 검색 안하면 안뜸 ㅠㅠ
-# 예를 들어 '존윅'이라고 하면 안뜨고 '존 윅'이라고 띄어쓰기까지 정확하게 해야 뜸
-search_and_save_movie_info('7c11fe750835c7c0b4c2bb32573c709f', '존 윅')
+search_and_save_movie_info('7c11fe750835c7c0b4c2bb32573c709f', '인터스텔라')
