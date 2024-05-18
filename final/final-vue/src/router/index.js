5ea54eb67806fd84087df456aaa27aa6abfcdaed
swybean <swybean@gmail.com>
2024-05-18 14:33:41 +0900

    5/18 14 33

--- final/final-vue/src/router/index.js
+++ final/final-vue/src/router/index.js
@@ -2,12 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '@/views/MainView.vue'
 import SignUpView from '@/views/SignUpView.vue'
 import LogInView from '@/views/LogInView.vue'
-
 import UserRecommandView from '@/views/UserRecommandView.vue'
 import UploadImage from '@/components/UploadImage.vue'
 import SharePost from '@/components/SharePost.vue'
 import CommunityView from '@/views/CommunityView.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import { useCounterStore } from '@/stores/counter'
 
 
 const router = createRouter({
@@ -28,25 +28,6 @@ const router = createRouter({
       name: 'LogInView',
       component: LogInView
     },
-  ]})
-
-  import { useCounterStore } from '@/stores/counter'
-
-  router.beforeEach((to, from) => {
-    const store = useCounterStore()
-    // 인증되지 않은 사용자는 메인 페이지에 접근 할 수 없음
-    if (to.name === 'MainView' && store.isLogin === false) {
-      window.alert('로그인이 필요해요!!')
-      return { name: 'LogInView' }
-    }
-  
-    // 인증된 사용자는 회원가입과 로그인 페이지에 접근 할 수 없음
-    if ((to.name === 'SignUpView' || to.name === 'LogInView') && (store.isLogin === true)) {
-      window.alert('이미 로그인 했습니다.')
-      return { name: 'MainView' }
-    }
-  })
-
     {
       path: '/recommand',
       name: 'UserRecommandView',
@@ -72,7 +53,29 @@ const router = createRouter({
       path: '/profile',
       name: 'ProfileView',
       component: ProfileView
-    }
-  ]})
+    },
+    
+    ]})
+    
+    router.beforeEach((to, from) => {
+      const store = useCounterStore()
+      // 인증되지 않은 사용자는 메인 페이지에 접근 할 수 없음
+      if (to.name === 'MainView' && store.isLogin === false) {
+        window.alert('로그인이 필요해요!!')
+        return { name: 'LogInView' }
+      }
+    
+      // 인증된 사용자는 회원가입과 로그인 페이지에 접근 할 수 없음
+      if ((to.name === 'SignUpView' || to.name === 'LogInView') && (store.isLogin === true)) {
+        window.alert('이미 로그인 했습니다.')
+        return { name: 'MainView' }
+      }
+      
+    })
+
+
+
+
+  
 
 export default router
